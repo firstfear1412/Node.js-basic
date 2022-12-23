21603512cb4b4b98e3863907b1f775ac6ebf5ac5
firstfear1412 <78691900+firstfear1412@users.noreply.github.com>
2022-12-23 21:13:46 +0700

    Lecture 10

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const debug = require('debug')('app');
 const app = express();
 const morgan = require('morgan');
 const port = 3000;
+const path = require('path')
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get('/',(req,res) => {
     res.send('Hello World test on Born to Dev Course');
